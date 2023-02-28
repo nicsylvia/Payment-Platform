f807869493d82dd87f69af4e78d1a4186215ca22
nicsylvia <nicsylvia15f@gmail.com>
2023-02-27 22:07:33 -0800

    started login

---
+++ src/Components/Auth/Login.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styled from 'styled-components';
+import green from "../Assets/green.png"
+import blue from "../Assets/blue.png"
+import purple from "../Assets/purple.png"
+import pink from "../Assets/pink.png"
+import logo from "../Assets/logo-white.svg"
