957a7775a8bf19ac95f5ec18158bb9ec3962f7a6
nicsylvia <nicsylvia15f@gmail.com>
2023-02-27 20:23:36 -0800

    working on piggyvest ui

--- src/Components/Auth/SignIn.tsx
+++ src/Components/Auth/SignIn.tsx
@@ -8,7 +8,7 @@ const SignIn = () => {
   return (
     <div>
         <Container>
-
+            <Wrapper></Wrapper>
         </Container>
     </div>
   )
@@ -19,9 +19,18 @@ export default SignIn;
 const Container = styled.div`
     width: 100%;
     height: 100vh;
+    /* padding: 10px 0px 10px 0px; */
     background-color: #062863;
     background-image: url(${blue}), url(${green}), url(${purple});
     background-repeat: no-repeat;
     background-size: 15%;
     background-position: left -1% top -15%, right -7.5% top -1%, right -5% bottom -5%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+const Wrapper = styled.div`
+    width: 70%;
+    padding: 20px 0px 30px 0px;
+    background-color: red;
 `;
