016b574dfd2a50b846c8e973c937dfe54ee6ead3
nicsylvia <nicsylvia15f@gmail.com>
2023-02-27 22:11:39 -0800

    still on login page

--- src/Components/Auth/Login.tsx
+++ src/Components/Auth/Login.tsx
@@ -43,3 +43,25 @@ const Login = () => {
 }
 
 export default Login;
+
+const Container = styled.div`
+    width: 100%;
+    padding: 10px 0px 10px 0px;
+    background-color: #062863;
+    background-image: url(${blue}), url(${green}),url(${purple}), url(${pink});
+    background-repeat: no-repeat;
+    background-size: 15%;
+    background-position: left -1% top -15%, right -7.5% top -1%, right -5% bottom -5%, left -5% bottom -5%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+const Wrapper = styled.div`
+    width: 40%;
+    padding: 20px 0px 30px 0px;
+    /* background-color: red; */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
