1a2082fa02b2a98aede3871118ca9f6854376e34
nicsylvia <nicsylvia15f@gmail.com>
2023-03-03 10:14:57 -0800

    saved all

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -13,5 +13,36 @@ const Header = () => {
       setShow(false)
     }
   }
+
+  window.addEventListener("scroll",changeHeaderColor)
+  return (
+    <div>
+      <Container boxShadow={show ? "value":""} id="header">
+        <Wrapper>
+          <Logo to="/">
+            <Img  src={logo}/>
+          </Logo>
+
+          <NavLink >
+            <Nav to="register">Save</Nav>
+            <Nav to ="">Invest</Nav>
+            <Nav to ="">Stories</Nav>
+            <Nav to="">FAQs</Nav>
+            <Nav to ="">Blog</Nav>
+            <Nav to="">E-Book</Nav>
+          </NavLink>
+
+          <ButtonHold>
+            <Button to="login" bg="white" cl="black" wd="100px" bcc='whitesmoke' ccl='#0D5ED4' >
+              Sign In
+            </Button>
+             <Button to="register" bg="black" cl="white" wd="220px" bcc='#0D5ED4' ccl=''>
+               Create Free Account
+             </Button>
+          </ButtonHold>
+        </Wrapper>
+      </Container>
+    </div>
+  )
 }
 export default Header
