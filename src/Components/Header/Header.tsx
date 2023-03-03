a794e014246a76979fb0f7281819d0f7c7072c90
nicsylvia <nicsylvia15f@gmail.com>
2023-03-03 10:12:24 -0800

    saved all

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link} from 'react-router-dom'
+import styled from 'styled-components'
+import logo from "../../Assets/logo.svg"
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [show,setShow] = React.useState(false)
 
+  const changeHeaderColor =()=>{
+    if(window.scrollY >= 1){
+      setShow(true)
+    }else{
+      setShow(false)
+    }
+  }
+}
 export default Header
