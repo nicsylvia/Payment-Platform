49656e6711ae94674d106038cbc61072acd12a76
nicsylvia <nicsylvia15f@gmail.com>
2023-03-03 11:29:26 -0800

    saved all

--- src/Components/HomeScreen/Chats.tsx
+++ src/Components/HomeScreen/Chats.tsx
@@ -30,4 +30,51 @@ const Chats:React.FC<props> = ({date,name,message,image}) => {
   )
 }
 
-export default Chats
+export default Chats;
+
+const Message = styled.div`
+margin: 0;
+font-size: 12px;
+letter-spacing: 1px;
+max-width: 400px;
+color: grey;
+`
+
+const Name = styled.div`
+font-size: 18px;
+color:grey;
+`
+
+const Card = styled.div`
+overflow: hidden;
+`
+
+const Wrapper = styled.div`
+display: flex;
+justify-content: center;
+`
+
+const Image = styled.img`
+width: 30px;
+height: 30px;
+margin-right: 10px;
+border-radius: 50%;
+`
+
+const Date = styled.div`
+color:gray;
+font-size: 15px;
+text-align: right;
+margin-right: 25px;
+`
+
+const Container = styled.div`
+display: flex;
+justify-content: center;
+width: 450px;
+flex-direction: column;
+border-radius: 10px;
+height: 200px;
+margin: 10px;
+background-color: #F9F9F9;
+`
