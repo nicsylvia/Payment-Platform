5ccc72784745f553a026c160dc005fbb42c840c7
nicsylvia <nicsylvia15f@gmail.com>
2023-03-03 16:04:15 -0800

    saved all

--- src/Components/ApiCalls/APIs.tsx
+++ src/Components/ApiCalls/APIs.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { UserData } from '../AllInterfaces/AllInterfaces';
+import axios from "axios";
 
-const APIs = () => {
-  return (
-    <div>APIs</div>
-  )
+const LocalURL = "http://localhost:3500"
+
+export const RegisterUser = async(data: UserData) =>{
+    await axios.post(`${LocalURL}/api/user/register`, data).then((res) =>{
+        res.data
+    })
 }
-
-export default APIs
